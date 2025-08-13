a1c8d2cd9eacd05db3ca08b48b421fdfc0e0b435
Sheema-goal-achiever <sheemagoalachiever@gmail.com>
2025-08-13 12:56:47 +0530

    Create rotate_right_by_k.java

---
+++ Leetcode/rotate_right_by_k.java
@@ -0,0 +1,31 @@
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(nums.length==0) return;
+        int n=nums.length;
+        k%=n;
+        swap(nums,0,n-1);
+        swap(nums,0,k-1);
+        swap(nums,k,n-1);
+    }
+    public void swap(int nums[],int left,int right)
+    {
+        while(left<=right)
+        {
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
+        }
+    }
+}
