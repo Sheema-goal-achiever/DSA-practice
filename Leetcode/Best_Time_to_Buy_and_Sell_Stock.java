2bc018e75147df1707784f75f589eebc5e2e12ef
Sheema-goal-achiever <sheemagoalachiever@gmail.com>
2025-08-13 13:02:42 +0530

    Create Best_Time_to_Buy_and_Sell_Stock.java

---
+++ Leetcode/Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,20 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different
+//   day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length , min=Integer.MAX_VALUE ,max=Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++)
+        {
+          if(prices[i]<min)
+          min=prices[i];
+          else
+          max=Math.max(max,prices[i]-min);
+        }
+
+        return max<0?0:max;
+    }
+}
